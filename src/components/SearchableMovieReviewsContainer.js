8082fc4303ff6ddc3d0b192956fd41fe8636fda4
Alexander <al.x.falconer@gmail.com>
2022-03-30 14:00:00 +0100

    search attempt

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,28 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    constructor() {
+      super();
+      this.state = {
+        reviews: [],
+        searchTerm: "",
+      };
+    }
+
+
+
+    render() {
+        return (
+          <div className="searchable-movie-reviews">
+            <div className="search-bar">
+              <h1>Search</h1>
+              <form onSubmit={this.sendSearch}>
+                <input
+                  className="form-control"
+                  id="search-input"
+                  type="text"
+                  onChange={this.handleChange}
+                  value={this.state.searchTerm}
+                  placeholder="Search for a movie"
+                />
